d6e08939471fed27571b90bd9f2eb946af98a76c
Fasasi Abdulrahman Temitope <106965053+Ramond-king05@users.noreply.github.com>
2022-12-05 08:40:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import neattext.functions as nfx
 from template import HTML_RANDOM_TEMPLATE,render_entities,get_tags,mytag_visualizer,plot_mendelhall_curve,plot_word_freq_with_altair,get_most_common_tokens
 
 
-st.set_page_config(page_icon="📖", page_title="Bible App")
+#st.set_page_config(page_icon="📖", page_title="Bible App")
 
 def load_bible(data): # function to load bible
 	df = pd.read_csv(data)
